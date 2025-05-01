23b243b6d9c6fefb362c575fb2efb27b6f534558
isaque-dantas <vicisaque413@gmail.com>
2025-05-01 10:34:35 -0300

    feat: setup alert system

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {HeaderComponent} from './components/header/header.component';
+import {AlertsContainerComponent} from './components/alerts-container/alerts-container.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, AlertsContainerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
