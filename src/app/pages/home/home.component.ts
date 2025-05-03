eed2c9aa9ebf2a82dcab1531b2efc4d81aaefc48
isaque-dantas <vicisaque413@gmail.com>
2025-05-03 13:46:07 -0300

    feat: add homepage

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
+import {RouterLink} from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  imports: [
+    NgOptimizedImage,
+    RouterLink
+  ],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
