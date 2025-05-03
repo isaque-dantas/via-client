600911221e5563b3906aa271b852b35091fe7bc4
isaque-dantas <vicisaque413@gmail.com>
2025-05-03 13:42:29 -0300

    feat: setup ngx-mask

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,11 +4,13 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import {provideHttpClient, withInterceptors} from '@angular/common/http';
 import {authInterceptor} from './interceptors/auth.interceptor';
+import {provideEnvironmentNgxMask} from 'ngx-mask';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideEnvironmentNgxMask(),
     provideHttpClient(
       withInterceptors([authInterceptor])
     ),
