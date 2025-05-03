1c65a85eb6d7ef628c378bd7e2344cb7114a86cf
isaque-dantas <vicisaque413@gmail.com>
2025-05-03 13:49:54 -0300

    feat: start page

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
+import {Employee} from '../../interfaces/employee';
+import {ActivatedRoute, Router} from '@angular/router';
+import {EmployeeService} from '../../services/employee.service';
+import {AlertService} from '../../services/alert.service';
+import {HttpErrorResponse} from '@angular/common/http';
 
 @Component({
   selector: 'app-profile',
@@ -6,6 +11,33 @@ import { Component } from '@angular/core';
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  employee?: Employee
 
+  private readonly route = inject(ActivatedRoute);
+  private readonly router = inject(Router);
+  employeeService = inject(EmployeeService)
+  alertService = inject(AlertService)
+
+  ngOnInit() {
+    this.route.params.subscribe((data: any) => {
+      const email = data.email
+
+      this.employeeService.get(email).subscribe({
+        next: employee => {
+          this.employee = employee
+          console.log(employee)
+        },
+        error: (response: HttpErrorResponse) => {
+          if (response.status === 404) {
+            this.alertService.error(`O usuário com e-mail \'${email}\' não foi encontrado.`)
+            this.router.navigateByUrl('/dashboard')
+            return;
+          }
+
+          this.alertService.error(response.message)
+        }
+      })
+    })
+  }
 }
