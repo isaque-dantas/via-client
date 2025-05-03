c35154d39758138b4d32abbc18fb59d5873783e2
isaque-dantas <vicisaque413@gmail.com>
2025-05-03 13:45:14 -0300

    feat: add routes for details

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import {Routes} from '@angular/router';
-import {DashboardComponent} from './pages/dashboard/dashboard.component';
+import {HomeComponent} from './pages/home/home.component';
 import {CustomerListComponent} from './pages/customer-list/customer-list.component';
 import {OrderListComponent} from './pages/order-list/order-list.component';
 import {OrderDetailComponent} from './pages/order-detail/order-detail.component';
@@ -8,19 +8,22 @@ import {LoginComponent} from './pages/login/login.component';
 import {ProfileComponent} from './pages/profile/profile.component';
 import {RegisterComponent} from './pages/register/register.component';
 import {loggedOnlyGuard} from './auth/logged-only.guard';
+import {CustomerDetailComponent} from './pages/customer-detail/customer-detail.component';
 
 export const routes: Routes = [
+  {path: 'home', component: HomeComponent},
   {path: 'register', component: RegisterComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'profile', component: ProfileComponent, canActivate: [loggedOnlyGuard]},
 
-  {path: 'dashboard', component: DashboardComponent, canActivate: [loggedOnlyGuard]},
-  {path: 'customers', component: CustomerListComponent, canActivate: [loggedOnlyGuard]},
+  {path: 'employee', component: ProfileComponent, canActivate: [loggedOnlyGuard]},
+  {path: 'employee/:email', component: ProfileComponent, canActivate: [loggedOnlyGuard]},
   {path: 'products', component: ProductListComponent, canActivate: [loggedOnlyGuard]},
-
+  {path: 'product/:id', component: ProductListComponent, canActivate: [loggedOnlyGuard]},
+  {path: 'customers', component: CustomerListComponent, canActivate: [loggedOnlyGuard]},
+  {path: 'customer/:id', component: CustomerDetailComponent, canActivate: [loggedOnlyGuard]},
   {path: 'orders', component: OrderListComponent, canActivate: [loggedOnlyGuard]},
   {path: 'order/:id', component: OrderDetailComponent, canActivate: [loggedOnlyGuard]},
 
   {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
-  {path: '**', component: DashboardComponent},
+  {path: '**', redirectTo: '/home', pathMatch: 'full'}
 ];
